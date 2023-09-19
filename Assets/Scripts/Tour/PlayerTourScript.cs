6b872c9a2d6d491c57e1f3c611a213811bb8c4ea
CptZee <68337605+CptZee@users.noreply.github.com>
2023-09-20 01:36:41 +0800

    Player Stuff
    Player can now move

---
+++ Assets/Scripts/Tour/PlayerTourScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTourScript : MonoBehaviour
+{
+    
+    private Renderer rend;
+    private Rigidbody2D rb;
+    public float moveSpeed = 5;
+    
+    private Animator animator;
+    
+    void Start()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
+
+        boxCollider.size = spriteRenderer.sprite.bounds.size;
+        boxCollider.offset = spriteRenderer.sprite.bounds.center;
+        
+        rb = GetComponent<Rigidbody2D>();
+        rend = GetComponent<Renderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+        public void MoveLeft()
+    {
+        float moveDirection = -1;
+        Debug.Log("Moving Left...");
+        animator.SetFloat("Vertical", moveDirection);
+        animator.SetFloat("Speed", moveSpeed);
+        rb.velocity = new Vector2(moveDirection * moveSpeed, 0);
+    }
+
+    public void MoveRight()
+    {
+        float moveDirection = 1;
+        Debug.Log("Moving Right...");
+        animator.SetFloat("Vertical", moveDirection);
+        animator.SetFloat("Speed", moveSpeed);
+        rb.velocity = new Vector2(moveDirection * moveSpeed, 0);
+    }
+
+    public void StopMoving()
+    {
+        Debug.Log("Standing...");
+        animator.SetFloat("Vertical", 0);
+        animator.SetFloat("Speed", 0);
+        rb.velocity = new Vector2(0, 0);
+    }
+}
