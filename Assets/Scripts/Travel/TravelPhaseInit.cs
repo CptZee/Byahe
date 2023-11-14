72ae6a7389cb783170e8cfb648c5ddedd72de438
CptZee <68337605+CptZee@users.noreply.github.com>
2023-11-14 21:41:39 +0800

    Updated gas logic

---
+++ Assets/Scripts/Travel/TravelPhaseInit.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TravelPhaseInit : MonoBehaviour
+{
+    public Slider gasSlider;
+    private DataManager datamanager;
+    void Start()
+    {
+        datamanager = DataManager.instance;
+        gasSlider.maxValue = datamanager.Gas;
+    }
+}
